e3d18f1e8db79fcb74afc86968938aea1961b22b
Aaron Mariano <Amarian10@gmail.com>
2016-07-06 12:32:28 -1000

    Added form parcel, and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,8 @@
 class ProfilesController < ApplicationController
 	def new
 	# form where a user can fill out their own profile
-	@user = User.find( params[:user_id] )
-	@profile = Profile.new
+		@user = User.find( params[:user_id] )
+		@profile = Profile.new
 	end
 
 
@@ -16,6 +16,12 @@ class ProfilesController < ApplicationController
 			render action: :new
 		end
 	end
+
+	def edit
+		@user = User.find( params[:user_id] )
+		@profile = @user.profile
+	end 
+
 	private 
 		def profile_params
 			params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
